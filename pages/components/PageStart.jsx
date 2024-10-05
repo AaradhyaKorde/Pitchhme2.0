77d24d28197d97576aaa327977b038d4cd7b316c
unknown <aaradhya.korde@gmail.com>
2024-10-05 21:17:01 +0530

    jsbj

--- pages/components/PageStart.jsx
+++ pages/components/PageStart.jsx
@@ -30,6 +30,13 @@ const satoshi = localFont({
 });
 
 const PageStartSection = (props) => {
+  // Add a check for ourApproachData
+  const approachData = ourApproachData[props.number] || {};
+  const helpData = helpSectionData[props.number] || {};
+  const servicesData = servicesIncluded[props.number] || {};
+  const resultsData = ourResults[props.number] || {};
+  const platformsData = platforms[props.number] || {};
+
   return (
     <div className={`${satoshi.variable} font-sans bg-black text-white min-h-screen flex flex-col relative overflow-hidden`}>
       <Navbar />
@@ -39,12 +46,40 @@ const PageStartSection = (props) => {
           {props.description}
         </p>
       </main>
-      <OurApproachSection description1={ourApproachData[props.number].description1} description2={ourApproachData[props.number].description2}/>
-      <WeCanHelpYou text1={helpSectionData[props.number].text1} text2={helpSectionData[props.number].text2} text3={helpSectionData[props.number].text3} text4={helpSectionData[props.number].text4} />
-      <ServicesIncluded service1={servicesIncluded[props.number].service1} service2={servicesIncluded[props.number].service2} service3={servicesIncluded[props.number].service3} service4={servicesIncluded[props.number].service4} service5={servicesIncluded[props.number].service5} service6={servicesIncluded[props.number].service6} service7={servicesIncluded[props.number].service7} service8={servicesIncluded[props.number].service8} />
+      <OurApproachSection 
+        description1={approachData.description1 || ''}
+        description2={approachData.description2 || ''}
+      />
+      <WeCanHelpYou 
+        text1={helpData.text1 || ''}
+        text2={helpData.text2 || ''}
+        text3={helpData.text3 || ''}
+        text4={helpData.text4 || ''}
+      />
+      <ServicesIncluded 
+        service1={servicesData.service1 || ''}
+        service2={servicesData.service2 || ''}
+        service3={servicesData.service3 || ''}
+        service4={servicesData.service4 || ''}
+        service5={servicesData.service5 || ''}
+        service6={servicesData.service6 || ''}
+        service7={servicesData.service7 || ''}
+        service8={servicesData.service8 || ''}
+      />
       <TheProcessWeFollow />
-      <OurResults result1={ourResults[props.number].result1} result2={ourResults[props.number].result2} result3={ourResults[props.number].result3} result4={ourResults[props.number].result4} />
-      <OurPlatforms link1={platforms[props.number].link1} link2={platforms[props.number].link2} link3={platforms[props.number].link3} link4={platforms[props.number].link4} link5={platforms[props.number].link5} />
+      <OurResults 
+        result1={resultsData.result1 || ''}
+        result2={resultsData.result2 || ''}
+        result3={resultsData.result3 || ''}
+        result4={resultsData.result4 || ''}
+      />
+      <OurPlatforms 
+        link1={platformsData.link1 || ''}
+        link2={platformsData.link2 || ''}
+        link3={platformsData.link3 || ''}
+        link4={platformsData.link4 || ''}
+        link5={platformsData.link5 || ''}
+      />
       <Footer />
     </div>
   );
